9105ab9ceab001383b2ac310672a3bc8ab47c20f
katarzyna_w <katarzyna.koch@gmail.com>
2021-09-17 16:21:09 -0500

    commit basic layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { Card } from './components/Card'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMeteor, faRocket } from '@fortawesome/free-solid-svg-icons'
+import HourglassIcon from '@material-ui/icons/HourglassEmpty'
+
+import TextField from '@material-ui/core/TextField'
+import DateAdapter from '@mui/lab/AdapterDateFns';
+import { LocalizationProvider } from '@mui/lab' 
+import DatePicker from '@mui/lab/DatePicker';
+
+
 import './App.css';
 
+
 function App() {
+
+  const [ data, setData ] = useState([])
+  // const [ image, setImage ] = useState()
+  // const [ title, setTitle ] = useState('')
+  // const [ date, setDate ] = useState('')
+  // const [ like, setLike ] = useState(false)   
+  // const [ userInput, setUserInput ] = useState('')
+  
+  const [ loading, setLoading ] = useState(false);
+  const [ value, setValue ] = useState('')
+  const nasaEndpoint=process.env.REACT_APP_NASA_ENDPOINT
+  const nasaApiKey=process.env.REACT_APP_NASA_API_KEY
+
+
+  useEffect (() => {
+    setLoading(true)
+    fetch(`${nasaEndpoint}planetary/apod/?api_key=${nasaApiKey}`)
+    .then((response) => response.json())
+    .then((data) => {
+      setData(data)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+    .finally(() => {
+      setLoading(false);
+    });
+  }, []);
+
+
+
+  // console.log("data media type: ", data.media_type)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <FontAwesomeIcon icon={ faMeteor } />
+        <span>Spacestagram</span>
+        <FontAwesomeIcon icon={ faRocket } />
       </header>
+      <div className="container-body">
+              <LocalizationProvider dateAdapter={DateAdapter}>
+                <DatePicker 
+                  label="Pick a date"
+                  value={value}
+                  onChange={(newValue) => {
+                    setValue(newValue);
+                  }}
+                  renderInput={(params) => <TextField {...params} />}
+                />
+              </LocalizationProvider>
+
+          { (data!==undefined) ? <Card data={data} /> : <div className='app-loading'><HourglassIcon/> Data is loading...</div> 
+          }
+      </div>
+        <footer>
+          Built by <a href="https://katarzyna-kw.github.io/portfolio-website/" target="blank"> Katarzyna Wegrzynowicz</a> using <a href="https://api.nasa.gov/" target="blank">NASA APOD API</a>
+        </footer>
     </div>
   );
 }
-
 export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from 'react'
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
+
+export const Card = ({ data }) => {
+   const [ selectedMedia, setSelectedMedia ] = useState(data.media_type);
+   // const [ image, setImage ] = useState()
+   // const [ title, setTitle ] = useState('')
+   // const [ date, setDate ] = useState('')
+
+   //reset media type when media changes
+   useEffect(()=> {
+      setSelectedMedia(data.media_type);
+   }, [data.media_type])
+
+   return (
+<>
+<div className="container-content">
+   <div className="container-img">
+      { (selectedMedia==="video") ? 
+      <div className="video-wrapper">
+      <iframe
+         width="100%"
+         height="100%"
+         src={data.url}
+         title="YouTube video"
+         frameBorder="0"
+         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen
+         >
+      </iframe>
+   </div> : 
+   <img src={data.hdurl} className="image" alt="logo" />
+}
+    </div>
+    <div className="container-img-info">
+      <div className="img-header">
+         <p className="title">
+          {data.title}
+         </p>
+         <FormControlLabel
+            control={<Checkbox icon={<FavoriteBorderIcon />} 
+            checkedIcon={<FavoriteIcon />}
+            name="liked" />}
+         />
+         <p className="count">1</p>
+      </div>
+      <p className="date">
+         {data.date}
+      </p>
+      <p className="caption">
+         {data.explanation}
+      </p>
+    </div>
+</div>
+</>
+)
+}
