f78b8366809d57c20e4213c35ac23a9d38a215f0
katarzyna_w <katarzyna.koch@gmail.com>
2022-01-14 14:35:30 -0600

    check

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ function App() {
 
   const [ selectedMedia, setSelectedMedia ] = useState([]);
   const [ loading, setLoading ] = useState(false);
-  // const nasaEndpoint=process.env.REACT_APP_NASA_ENDPOINT
+  const nasaEndpoint=process.env.REACT_APP_NASA_ENDPOINT
   const nasaApiKey=process.env.REACT_APP_NASA_API_KEY
 
 
   useEffect (() => {
     setLoading(true)
-    fetch(`https://api.nasa.gov/planetary/apod/?api_key=${nasaApiKey}`)
+    fetch(`${nasaEndpoint}planetary/apod/?api_key=${nasaApiKey}`)
     .then((response) => response.json())
     .then((data) => {
       setSelectedMedia(data)
@@ -25,13 +25,13 @@ function App() {
     .finally(() => {
       setLoading(false)
     });
-  }, [nasaApiKey]);
+  }, [nasaEndpoint, nasaApiKey]);
 
   return (
     <div className="App-body" data-testid="app-body">
       <Header data-testid="header"/>
       <Main data-test id="main" 
-        // nasaEndpoint={nasaEndpoint} 
+        nasaEndpoint={nasaEndpoint} 
         nasaApiKey={nasaApiKey} 
         onSelect={setSelectedMedia}
         loadingMedia={setLoading}
